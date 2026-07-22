6775e2969fbe5dcf0d2bef66c766333e136f1d8a
fedoran2 <fedoran2.cvut.cz>
2026-07-22 14:30:36 +0200

    Setup backend skeleton

---
+++ backend/public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/api/health', function (Request $request, Response $response): Response {
+    $response->getBody()->write(json_encode([
+        'status' => 'ok',
+    ]));
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
